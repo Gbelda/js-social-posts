94bf9f4a9f6c96ff5c3bc8af83a2e69cd1d8192d
gbelda <gbelda56@gmail.com>
2021-10-25 16:12:59 +0200

    Created base card structure

--- ASSETS/JS/main.js
+++ ASSETS/JS/main.js
@@ -2,3 +2,17 @@
 /* Gli attributi minimi del modello di un post: id, contenuto, immagine,
 autore(nome, avatar), numero di likes, data creazione. */
 //Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
+
+let post = [
+    {
+        postId: 'post1',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, ut.',
+        image: 'https://unsplash.it/300/300?image=',
+        userName: "Lisa Manoban",
+        userPicture: "https://otakukart.com/wp-content/uploads/2021/07/lisa.jpg",
+        likes: 'x likes',
+        datePosted: '2 months ago'
+
+    }
+]
+
